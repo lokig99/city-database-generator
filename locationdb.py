24c2bf83a13c4b222a14e7c5cc3e357b792e5608
Mateusz Groblicki <lokigameplayer@gmail.com>
2020-10-17 16:19:10 +0200

    performance improvements

--- locationdb.py
+++ locationdb.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import os
 import re
 import json
@@ -59,15 +61,30 @@ class City(Location):
         self.longitude = longitude
 
     def __str__(self):
-        s = super().__str__()
-        return f"{s} {self.latitude} {self.longitude}"
+        return f"{super().__str__()}, {self.country}"
+
+    def __degrees_to_decimal(self, coordinates: Tuple[float]) -> float:
+        decimal = 0.0
+        mult = 1
+        for dd in coordinates[1:]:
+            mult *= 60
+            decimal += dd / mult
+        if coordinates[0] < 0:
+            return coordinates[0] - decimal
+        return coordinates[0] + decimal
+
+    def latitude_decimal(self):
+        return self.__degrees_to_decimal(self.latitude)
+
+    def longitude_decimal(self):
+        return self.__degrees_to_decimal(self.longitude)
 
 
 class LocationDatabase:
     def __init__(self):
         self.__is_opened = False
-        self.__cities_indexed: Dict[str, List[City]] = dict(
-            [(l, []) for l in ASCII_ALPHABET])
+        self.__cities_indexed: Dict[str, Dict[str, List[City]]] = dict(
+            [(l, {}) for l in ASCII_ALPHABET])
         self.__countries: Set[Country] = set()
 
     def open_from_json(self) -> bool:
@@ -104,8 +121,13 @@ class LocationDatabase:
                     if len(tmp_dict) == len(keys):
                         city = City(tmp_dict[NAME], country,
                                     tmp_dict[LAT], tmp_dict[LON])
-                        self.__cities_indexed[city.searchable_name_normalized()[0]].append(
-                            city)
+                        header = city.searchable_name_normalized()[:2]
+                        if header[1] in self.__cities_indexed[header[0]]:
+                            self.__cities_indexed[header[0]
+                                                  ][header[1]].append(city)
+                        else:
+                            self.__cities_indexed[header[0]
+                                                  ][header[1]] = [city]
                         tmp_dict.clear()
                     tmp_dict[keys[i]] = data_bit
             result = True
@@ -122,18 +144,21 @@ class LocationDatabase:
                 city_name = city.searchable_name()
             return city_name.startswith(text)
 
-        matches = set()
+        matches: Set[City] = set()
         if len(text) > 0 and text[0].isalpha():
             text = _normalize_text(text, keep_diacritics=True)
-            fst_letter = _normalize_text(text[0])
-            for city in self.__cities_indexed[fst_letter]:
-                if text_in_city_attr(text, city):
-                    matches.add(city)
+            header = _normalize_text(text[:2])
+            if len(header) < 2:
+                for _, cities in self.__cities_indexed[header[0]].items():
+                    matches.update(cities)
+            else:
+                try:
+                    for city in self.__cities_indexed[header[0]][header[1]]:
+                        if text_in_city_attr(text, city):
+                            matches.add(city)
+                except KeyError:
+                    pass
         return matches
-
-
-def printable_matches(matches: Set[City]) -> List[str]:
-    return [f'{item.name}, {item.country.name}\tLat:{item.latitude}, Lon:{item.longitude}' for item in matches]
 
 
 def main():
@@ -152,8 +177,8 @@ def main():
         start = time.time()
         matches = db.search(searched_text)
         end = time.time()
-        for match in printable_matches(matches):
-            print(match)
+        for city in matches:
+            print(city)
         print(f"\nSearch took: {round((end - start) * 1000)} miliseconds\n")
 
 
